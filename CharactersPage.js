862d1804c50175f129ec785757949fe0255242be
Gustavo <gugacontas1303@gmail.com>
2024-11-18 23:22:44 -0300

    feat: adding navigation between pages

---
+++ CharactersPage.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+    $('.menu_list_link').click(function() {
+        event.preventDefault();
+
+        window.location.href = 'home.html';  
+    });
+});
---
+++ home.js
@@ -0,0 +1,6 @@
+
+$(document).ready(function() {
+    $('.character_card').click(function() {
+        window.open('CharactersPage.html', '_blank');   
+    });
+});
