3ac51c6a166ec1797776d9a156c12b378588b244
Gustavo Barreto <Gugacontas1303@gmail.com>
2025-01-16 00:38:48 -0300

    feat: Integrate API on the first page

--- home.js
+++ home.js
@@ -9,47 +9,46 @@ $(document).ready(function() {
     const publicKey = "15cea5f8a59b71e7d636187219977051";
     const hash = "62eb37bec60ace73acd483210bc3fc51";
 
-    async function fetchHeroes() {
-
-        const url = `http://gateway.marvel.com/v1/public/characters?ts=${ts}&apikey=${publicKey}&hash=${hash}`;
-        
-        try {
-            const response = await fetch(url);
-            const data = await response.json();
-
-            if (data.code === 200) {
-            console.log(data.data.results); 
-            displayHeroes(data.data.results); 
-            } else {
-            console.error("Erro na API:", data.status);
-            }
-        } catch (error) {
-            console.error("Erro na requisição:", error);
-        }
-    }
-
-    function displayHeroes(heroes) {
-
-        const container = document.getElementById("hero-container");
-        container.innerHTML = ""; 
-    
-        heroes.forEach(hero => {
-        
-        const card = `
-            <div class="card">
-            <img src="${hero.thumbnail.path}.${hero.thumbnail.extension}" alt="${hero.name}">
-            <h3>${hero.name}</h3>
-            <button onclick="goToHeroPage(${hero.id})">Ver mais</button>
-            </div>
-        `;
-        container.innerHTML += card; 
-        });
-    }
-
-    $('.character_card').click(function() {
-        const character = $(this).attr('id');
-        window.open(`CharactersPage.html?hero=${character}`, '_blank');   
-    });
-
+    fetchHeroes(ts , publicKey , hash); 
 
 });
+
+/* FUNCTIONS */
+
+async function fetchHeroes(ts , publicKey , hash) {
+
+  const url = `http://gateway.marvel.com/v1/public/characters?ts=${ts}&apikey=${publicKey}&hash=${hash}`;
+  
+  try {
+      const response = await fetch(url);
+      const data = await response.json();
+
+      if (data.code === 200) {
+      console.log(data.data.results); 
+      displayHeroes(data.data.results); 
+      } else {
+      console.error("Erro na API:", data.status);
+      }
+  } catch (error) {
+      console.error("Erro na requisição:", error);
+  }
+}
+
+function displayHeroes(heroes) {
+
+  const container = document.getElementById("hero_container");
+  container.innerHTML = ""; 
+
+  heroes.forEach(hero => {
+
+      const card = `
+      <div class="character_card" id="${hero.name.toLowerCase().replace(/\s+/g, '-')}" data-character="${hero.name}">
+        <img src="${hero.thumbnail.path}.${hero.thumbnail.extension}" alt="${hero.name}">
+        <div class="character_card_content">
+          <h1 class="character_card_content_title">${hero.name}</h1>
+        </div>
+      </div>
+    `;
+    container.innerHTML += card; 
+  });
+}
